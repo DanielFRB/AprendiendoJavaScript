76e2acae1945b92652f9ecd6528c75a3a3d9d657
Daniel Rondon <danfelrondon@gmail.com>
2022-06-28 22:01:14 -0400

    Vanilla JS SPA: Componente SearchForm.js y Actualizacion de styles.css

---
+++ FundamentosJS/spa/app/components/SearchForm.js
@@ -0,0 +1,9 @@
+export function SearchForm() {
+  const d = document, $form = d.createElement("form"), $input = d.createElement("input");
+  $form.classList.add("form-search");
+  $input.name = "search";
+  $input.type = "search";
+  $input.placeholder = "Buscar..."
+  $form.appendChild($input);
+  return $form;
+}
