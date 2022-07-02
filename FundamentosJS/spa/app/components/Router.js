bca5c893a352804fa3f8a2456e7db3ccd093ff0b
Daniel Rondon <danfelrondon@gmail.com>
2022-07-02 13:02:14 -0400

    Vanilla JS SPA: Buscador Interno y Mostrar Vista del Resultado de la Busqueda Actual

--- FundamentosJS/spa/app/components/Router.js
+++ FundamentosJS/spa/app/components/Router.js
@@ -2,6 +2,7 @@ import { ajax } from "../helpers/ajax.js"
 import api from "../helpers/wp_api.js"
 import { Post } from "./Post.js";
 import { PostCard } from "./PostCard.js";
+import { SearchCard } from "./SearchCard.js";
 
 export async function Router() {
   const d = document,
@@ -15,12 +16,28 @@ export async function Router() {
       url : api.POSTS,
       cbSuccess : (posts) => {
         let html = "";
-        posts.forEach((post) => html += PostCard(post));
+        posts.forEach((post) => {html += PostCard(post)});
         $main.innerHTML = html;
       }
     });
   } else if (hash.includes("#/search")) {
-    $main.innerHTML = "<h2>Buscador</h2>"
+    let query = localStorage.getItem("wpSearch");
+    if (!query) {
+      d.querySelector(".loader").style.display = "none"; 
+      return false;
+    };
+    await ajax({
+      url : `${api.SEARCH}${query}`,
+      cbSuccess : (search) => {
+        let html = "";
+        if (search.length === 0) {
+          html = `<p class="error">No existen resultados que concuerden con el termino: <mark>${query}</mark></p>`;
+        } else {
+          search.forEach((post) => {html += SearchCard(post)});
+        };
+        $main.innerHTML = html;
+      }
+    });
   } else if (hash === "#/contact") {
     $main.innerHTML = "<h2>Contacto</h2>"
   } else {
--- FundamentosJS/spa/app/components/SearchForm.js
+++ FundamentosJS/spa/app/components/SearchForm.js
@@ -1,9 +1,29 @@
 export function SearchForm() {
   const d = document, $form = d.createElement("form"), $input = d.createElement("input");
+
   $form.classList.add("search-form");
   $input.name = "search";
   $input.type = "search";
-  $input.placeholder = "Buscar..."
+  $input.placeholder = "Buscar...";
+  $input.autocomplete = "off";
+
   $form.appendChild($input);
+
+  if (location.hash.includes("#/search")) {
+    $input.value = localStorage.getItem("wpSearch");
+  };
+
+  d.addEventListener("search", (e) => {
+    if (!e.target.matches("input[type='search']")) return false;
+    if (!e.target.value) localStorage.removeItem("wpSearch");
+  });
+
+  d.addEventListener("submit", (e) => {
+    if (!e.target.matches(".search-form")) return false;
+    e.preventDefault();
+    localStorage.setItem("wpSearch", e.target.search.value);
+    location.hash = `#/search?search=${e.target.search.value}`;
+  });
+
   return $form;
 };
